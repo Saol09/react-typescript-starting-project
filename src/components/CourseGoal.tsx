380bc44b2a16a69c1ef68771a05844718a8b5dba
Lopes <sergiolopes@deloitte.pt>
2023-12-08 18:45:25 +0000

    First commit with some components

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,35 @@
+import { type PropsWithChildren /* , type ReactNode */ } from "react";
+
+// Alternative from type below
+/* 
+type CourseGoalProps = {
+  title: string;
+  children: ReactNode;
+}; */
+
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
+
+export default function CourseGoal({ title, children }: CourseGoalProps) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
+
+// another way to do a function
+/* const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}; */
